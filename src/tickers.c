2f24f3bce3e9925bcce1f90686628589898e79b4
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-05-22 08:13:02 -0400

    little input validation needed. Includes print statement if co symbol not found.

--- src/tickers.c
+++ src/tickers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 enum { MAX_CO_SIZE = 100, MAX_SYM_SIZE = 5, MAX_NO_CO = 10 };
 
@@ -31,6 +32,7 @@ int main(void)
 		create_companies(curr_co, &co_list[i]);
 		i++;
 	}
+        fclose(fp);
 
 	print_ticker(co_list);
 
@@ -39,7 +41,6 @@ int main(void)
 
 long get_file_size(char file[])
 {
-
 	FILE *fp = fopen(file, "r");
 	fseek(fp, (long)0, SEEK_END);
 	long file_size = ftell(fp);
@@ -50,14 +51,9 @@ long get_file_size(char file[])
 void create_companies(char *curr_co, company * co_list)
 {
 	char *tmp;
-	// int nl_loc = 0;
-	// strcspn(symbol, "\n");
 
 	tmp = strsep(&curr_co, ":");
-	// nl_loc = strcspn(tmp, "\n");
-	// tmp[nl_loc] = '\0';
 	strncpy(co_list->symbol, tmp, sizeof(co_list->symbol));
-	// strncpy(co_list->symbol, tmp, nl_loc + 1);
 	tmp = strsep(&curr_co, "\n");
 	strncpy(co_list->co_name, tmp, sizeof(co_list->co_name));
 }
@@ -66,18 +62,23 @@ void print_ticker(company co_list[])
 {
 	int nl_loc = 0;
 	char symbol[MAX_SYM_SIZE] = { 0 };
+        bool co_found;
 
 	printf("Enter a symbol to search by: ");
 	fgets(symbol, MAX_SYM_SIZE, stdin);
 
 	nl_loc = strcspn(symbol, "\n");
 	symbol[nl_loc] = '\0';
-	// with scanf, symbol for nike = "NKE\n"
-	// with fgets, symbol for nike = "NKE\0"
 
 	for (int i = 0; i < MAX_NO_CO; ++i) {
+                co_found = false;
 		if ((strncmp(co_list[i].symbol, symbol, sizeof(symbol)) == 0)) {
-			printf("%s\n", co_list[i].co_name);
+			printf("%s: %s\n\n", co_list[i].symbol, co_list[i].co_name);
+                        co_found = true;
+                        break;
 		}
 	}
+        if (!co_found) {
+                printf("No company found with provided symbol\n");
+        }
 }
