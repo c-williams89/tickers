3081fdea3a95196d0669d9871adaf48acaeb119e
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-05-23 08:42:32 -0400

    code cleanup, used #define for file names.

--- src/tickers.c
+++ src/tickers.c
@@ -2,17 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-// #include <ctype.h>
 #include <stdio_ext.h>
 
-enum { MAX_CO_SIZE = 100, MAX_SYM_SIZE = 5, MAX_NO_CO = 10 };
+#define INPUT_FILE "./data/tickers.txt"
+#define OUTPUT_FILE "./data/search_results"
+
+enum { MAX_CO_SIZE = 64, MAX_SYM_SIZE = 8, MAX_NO_CO = 10 };
 
 typedef struct company {
 	char symbol[5];
 	char co_name[50];
 } company;
 
-long get_file_size(char[]);
+long get_file_size(const char[]);
 
 void create_companies(char *curr_co, company * co_list);
 
@@ -22,19 +24,18 @@ void write_to(char *co_to_print);
 
 int main(void)
 {
-        system("clear");
+	system("clear");
 	company co_list[MAX_NO_CO] = { 0 };
-	char file[] = "./data/tickers.txt";
+	const char file[] = INPUT_FILE;
 	long file_size = get_file_size(file);
 
 	FILE *fp;
 	fp = fopen(file, "r");
-        if (!fp) {
-                perror("Could not open file: 'tickers.txt'\n");
-                exit(1);
-        }
+	if (!fp) {
+		perror("Could not open file: 'tickers.txt'\n");
+		exit(1);
+	}
 	char curr_co[MAX_CO_SIZE];
-
 	int i = 0;
 	while (fgets(curr_co, file_size, fp)) {
 		curr_co[strlen(curr_co)] = '\0';
@@ -42,17 +43,16 @@ int main(void)
 		i++;
 	}
 	fclose(fp);
-
 	print_ticker(co_list);
 }
 
-long get_file_size(char file[])
+long get_file_size(const char file[])
 {
 	FILE *fp = fopen(file, "r");
-        if (!fp) {
-                perror("Could not open file: 'tickers.txt'\n");
-                exit(1);
-        }
+	if (!fp) {
+		perror("Could not open file: 'tickers.txt'\n");
+		exit(1);
+	}
 	fseek(fp, (long)0, SEEK_END);
 	long file_size = ftell(fp);
 	fclose(fp);
@@ -62,7 +62,6 @@ long get_file_size(char file[])
 void create_companies(char *curr_co, company * co_list)
 {
 	char *tmp;
-
 	tmp = strsep(&curr_co, ":");
 	strncpy(co_list->symbol, tmp, sizeof(co_list->symbol));
 	tmp = strsep(&curr_co, "\n");
@@ -78,16 +77,15 @@ void print_ticker(company co_list[])
 	while (true) {
 		printf("Enter a symbol to search or 'Q' to quit: ");
 		fgets(symbol, MAX_SYM_SIZE, stdin);
-
 		nl_loc = strcspn(symbol, "\n");
 		symbol[nl_loc] = '\0';
-                for (int i = 0; symbol[i] != '\0'; ++i) {
-                        if (symbol[i] >= 'a' && symbol[i] <= 'z') {
-                                symbol[i] -= 32;
-                        }
-                }
+		for (int i = 0; symbol[i] != '\0'; ++i) {
+			if (symbol[i] >= 'a' && symbol[i] <= 'z') {
+				symbol[i] -= 32;
+			}
+		}
 		if (strncmp(symbol, "Q", 1) == 0) {
-                        printf("\n\tExiting now. Goodbye\n");
+			printf("\n\tExiting now. Goodbye\n");
 			exit(0);
 		} else {
 			for (int i = 0; i < MAX_NO_CO; ++i) {
@@ -99,7 +97,8 @@ void print_ticker(company co_list[])
 						 "%s: %s\n",
 						 co_list[i].symbol,
 						 co_list[i].co_name);
-                                        printf("\033[96m\t%s\033[0m\n\n", co_list[i].co_name);
+					printf("\033[96m\033[1m\t%s\033[0m\n\n",
+					       co_list[i].co_name);
 					co_found = true;
 					write_to(co_to_print);
 					break;
@@ -110,14 +109,13 @@ void print_ticker(company co_list[])
 				    ("\033[31m\033[1mNo company found with provided symbol\033[0m\n\n");
 			}
 		}
-                __fpurge(stdin);
+		__fpurge(stdin);
 	};
-
 }
 
 void write_to(char *co_to_print)
 {
-	char output_file[] = "./data/search_results";
+	char output_file[] = OUTPUT_FILE;
 	FILE *fp = fopen(output_file, "a");
 	fwrite(co_to_print, sizeof(char), strlen(co_to_print), fp);
 	fclose(fp);
